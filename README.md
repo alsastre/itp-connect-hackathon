a52f079689f7a492242a813c742f98e3b70a8c48
Alsastre <alsastre94@gmail.com>
2019-04-04 00:39:23 +0200

    Update README, added requirements, fixed project.py

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # itp-connect-hackathon
 itp-connect-hackathon
+
+
+## Setup
+1. Install Python3.5
+2. Create a virtualenv
+`virtualenv -p /usr/bin/python3.5 .env`
+Where `/usr/bin/python3.5` is the path to your python binary
+3. Activate environment
+`source .env/bin/activate`
+4. Install dependencies
+`pip install -r requirements.txt`
+5. Run the project from the root folder
+`python src/project.py`
--- src/project.py
+++ src/project.py
@@ -1,6 +1,10 @@
 import pandas as pd
-df = pd.read_csv("data/laundromat.csv", low_memory=False, )
-df_isna = df.isna()
-result = df[(df_isna["payer_name"] == False) & (df_isna["beneficiary_name"] == False) & ((df_isna["payer_bank"] == False) | (df_isna["beneficiary_bank"] == False)]
 
+def main():
+  df = pd.read_csv("data/laundromat.csv", low_memory=False, )
+  df_isna = df.isna()
+  result = df[(df_isna["payer_name"] == False) & (df_isna["beneficiary_name"] == False) & ((df_isna["payer_bank"] == False) | (df_isna["beneficiary_bank"] == False))]
+  print(result)
 
+if __name__ == "__main__":
+    main()
